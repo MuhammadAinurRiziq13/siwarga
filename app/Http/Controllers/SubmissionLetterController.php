9e15f91551c2d26c7eb4b11e5752ad7203b40f23
tektungg <2241720128@student.polinema.ac.id>
2024-06-04 08:36:17 +0700

    fix surat pengantar

--- app/Http/Controllers/SubmissionLetterController.php
+++ app/Http/Controllers/SubmissionLetterController.php
@@ -144,7 +144,7 @@ class SubmissionLetterController extends Controller
     {
         $pengajuan = SubmissionLetterModel::findOrFail($id);
 
-        $templateProcessor = new TemplateProcessor(storage_path('app/Templates/test.docx'));
+        $templateProcessor = new TemplateProcessor(storage_path('app/Templates/Pengantar.docx'));
 
         // Set data dari database ke dalam template
         $templateProcessor->setValue('nama', $pengajuan->nama);
