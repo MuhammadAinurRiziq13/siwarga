50b9ea77ef00bc9f11e12366ca8b2b3a8c0ad5b2
Alhamdanaaa <aalhamdana@gmail.com>
2024-06-06 21:44:32 +0700

    pengscoran

--- app/Http/Controllers/PoorFamilyController.php
+++ app/Http/Controllers/PoorFamilyController.php
@@ -103,7 +103,7 @@ class PoorFamilyController extends Controller
             // Temukan atau buat instansi PoorFamilyModel
             $poorFamily = PoorFamilyModel::firstOrNew(['noKK' => $family->noKK]);
             // Perbarui skor
-            $poorFamily->score = $ranking['score'];
+            $poorFamily->score = round($ranking['score'],4);
             // Simpan perubahan
             $poorFamily->save();
 
